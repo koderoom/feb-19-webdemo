a7b761098102e9258b94f42ce72d7f11e3c14268
santosh <santosh.ece06@gmail.com>
2019-05-22 14:12:43 +0530

    day 2

---
+++ day2/7.js
@@ -0,0 +1,16 @@
+console.log("Hello World, 7.js file");
+
+
+/** Add More Block */
+function addMoreBlock() {
+
+    // SINGLE LINE COMMENT
+    // let counter = 100;
+
+    console.log("Hello I am there!!!");
+}
+
+/** Delete Block */
+function deleteBlock() {
+
+}
