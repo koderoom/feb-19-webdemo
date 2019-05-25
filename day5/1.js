0fb030c8db8ce9665cdb1594e422c99ce384e965
santosh <santosh.ece06@gmail.com>
2019-05-25 13:55:33 +0530

    day 5

---
+++ day5/1.js
@@ -0,0 +1,67 @@
+
+/** POST USING DOCUMENT OBJECT */
+function postHere() {
+    let parent = document.querySelector("#blockParent");
+
+    let refBlock = parent.children[0];
+
+    let newBlock = refBlock.cloneNode(true);
+
+    // READ INPUT
+    let postElement = document.querySelector("#postid");
+    let postValue = postElement.value;
+
+    // ADD THE VALUE TO NEW BLOCK
+    newBlock.children[0].innerHTML = postValue;
+
+    // ADDING NEW BLOCK
+    parent.insertBefore(newBlock, parent.firstChild);
+
+    // clear the box;
+    postElement.value = "";
+}
+
+
+function postHereUsingJQuery() {
+    let parentBlock = $("#blockParent");
+
+    // GET FIRST CHILD :: REF BLOCK
+    let refBlock = parentBlock.children().first();
+
+    // CREATE NEW BLOCK
+    let newBlock = refBlock.clone(true);
+    let postValue = $("#postid").val();
+    newBlock.children().first().html(postValue);
+
+    parentBlock.append(newBlock);
+
+    // clear the box;
+    $("#postid").val("");
+}
+
+
+/** POSTING AN IMAGE */
+function postImage() {
+    console.log("POST IMAGE LOGIC");
+
+    let fileElement = document.querySelector("#fileId1");
+    let uploadFile = fileElement.files[0];
+
+    // MAKE A PROPER URL WITH THE UPLOADED IMAGE
+    let uploadFileAsURL = URL.createObjectURL(uploadFile);
+
+
+    // TEMP OPERATION : CHANGE THE EXISTING IAMGE
+    /*let imgElem = document.querySelector("img");
+    imgElem.src = uploadFileAsURL;*/
+
+    let imgRef = document.querySelector("#imgRefBlockId");
+    let newImgBlock = imgRef.cloneNode(true);
+
+    // chagne the source of image :: new block
+    newImgBlock.children[0].children[0].src = uploadFileAsURL;
+
+    // Attach the new block with parent;
+    let parent = document.querySelector("#blockParent");
+    parent.insertBefore(newImgBlock, parent.firstChild);
+}
